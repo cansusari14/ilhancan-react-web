4ccb1d7d6124422ef724c27ee70f4f611a14fdc4
cansusari14 <63806074+cansusari14@users.noreply.github.com>
2022-02-21 16:08:47 +0100

    Styling changes

--- components/Navigation.js
+++ components/Navigation.js
@@ -2,8 +2,8 @@ import styles from './Navigation.module.css'
 export default function Navigation() {
     return (
       <div className={styles.navigation}>
-        <a href="/about">About</a><br />
-        <a href="/song">Work</a>
+        <a href="/about">ABOUT</a><br />
+        <a href="/song">WORK</a>
       </div>
       
     )
